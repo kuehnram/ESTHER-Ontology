d03024eae0e540df2f6af5d1d2ebf18a55153d91
kuehnram <kuehn.ramona15@gmail.com>
2022-10-13 17:50:41 +0200

    Add files via upload

---
+++ FyF/ESTHEREnums.py
@@ -0,0 +1,43 @@
+from enum import Enum
+
+
+class Operation(Enum):
+    ADDITION = "Addition"
+    OMISSION = "Omission"
+    REPETITION = "Repetition"
+    CONVERSION = "Conversion"
+    REPLACEMENT = "Replacement"
+    NONE = "None"
+    QM = "?"
+
+
+class LinguisticElement(Enum):
+    LETTER = "Letter"
+    VOWEL = "Vowel"
+    CONSONANT = "Consonant"
+    WORD = "Word"
+    SENTENCE = "Sentence"
+    PHRASE = "Phrase"
+    VERSE = "Verse"
+    PUNCTUATION = "Punctuation"
+    NONE = "None"
+    QM = "?"
+
+
+class LinguisticObject(Enum):
+    SAME_FORM = "Same form"
+    DIFFERENT_FORM = "Different form"
+    SAME_MEANING = "Same meaning"
+    DIFFERENT_MEANING = "Different meaning"
+    OPPOSED_MEANING = "Opposed meaning"
+    NONE = "None"
+    QM = "?"
+
+
+class Position(Enum):
+    BEGINNING = "Beginning"
+    END = "End"
+    MIDDLE = "Middle"
+    BEGINNING_AND_END = "Beginning and end"
+    WHOLE = "Whole"
+    QM = "?"
