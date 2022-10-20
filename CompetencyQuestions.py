5f5979c36a620d4a7e235a8e29a3001da8e82199
Ramona Kühn <kuehn.ramona15@gmail.com>
2022-10-20 15:56:04 +0200

    competency question reformulated

--- CompetencyQuestions.py
+++ CompetencyQuestions.py
@@ -8,16 +8,16 @@ This file contains the competency questions (Grüninger and Fox 1995) to evaluat
 figures in English.
 Q1 and Q2 are the same as used by the RetFig and GRhOOT ontology for comparison.
 
-Q1: Rhetorical figures that have their defining element in a word.
-Q2: Rhetorical figures where a letter is omitted.
-Q3: Figures that are neither figures of construction, nor figures of speech, nor figures of thought.
-Q4: All figures that are tropes (should be same output as Q3).
-Q5: Example sentences for the figure Alliteration.
-Q6: Definition of the figure Parallelism.
-Q7: Figures of speech or figures of thought with position beginning.
-Q8: Figures with a repeated element of the same form.
-Q9: Figures where a word in the same form is repeated.
-Q10: Figures and their opposite figures.
+Q1: Which rhetorical figures have their defining element in a word?
+Q2: In which rhetorical figures is a letter omitted?
+Q3: What are figures that are neither figures of construction, nor figures of speech, nor figures of thought?
+Q4: Which figures belong to the rhetorical group of tropes (should be the same output as Q3)?
+Q5: What is an example sentence for the figure alliteration?
+Q6: What is the definition of the figure parallelism?
+Q7: Which figures of speech or figures of thought have their defining element in the beginning?
+Q8: Which figures have  repeat an element in the same form?
+Q9: In which figures is a word in the same form repeated?
+Q10: Which figures are the opposite of other figures?
 """
 
 import rdflib
@@ -27,7 +27,7 @@ g.parse('esther.owl', format='application/rdf+xml')
 esther = rdflib.Namespace('https://anonymous.org/')
 g.bind('esther', esther)
 
-# Q1: Rhetorical figures that have their defining element in a word.
+# Q1: Which rhetorical figures have their defining element in a word?
 cq1 = """
 SELECT distinct ?Figure
 WHERE {
@@ -36,7 +36,7 @@ WHERE {
     Filter (?AreaName = "Word" ) }
 """
 
-# Q2: Rhetorical figures where a letter is omitted.
+# Q2: In which rhetorical figures is a letter omitted?
 cq2 = """
 SELECT distinct ?Figure
 WHERE {
@@ -45,7 +45,7 @@ WHERE {
     Filter (?ElementName = "Letter" ) }
 """
 
-# Q3: Figures that are neither figures of construction, nor figures of speech, nor figures of thought
+# Q3: What are figures that are neither figures of construction, nor figures of speech, nor figures of thought?
 cq3 = """
 SELECT distinct ?Figure
 WHERE {
@@ -54,7 +54,7 @@ WHERE {
     Filter (?GroupName != "FigureOfConstruction" && ?GroupName != "FigureOfThought" && ?GroupName != "FigureOfSpeech") }
 """
 
-# Q4: All figure that are tropes (should be same output as Q3).
+# Q4: Which figures belong to the rhetorical group of tropes (should be the same output as Q3)?
 cq4 = """
 SELECT distinct ?Figure
 WHERE {
@@ -63,7 +63,7 @@ WHERE {
     Filter (?GroupName = "Trope") }
 """
 
-# Q5: Example sentences for the figure Alliteration.
+# Q5: What is an example sentence for the figure alliteration?
 cq5 = """
 SELECT distinct ?Figure ?Example ?Value
 WHERE {
@@ -72,7 +72,7 @@ WHERE {
     Filter (?FigureName = "Alliteration") }
 """
 
-# Q6: Definition of figure Parallelism.
+# Q6: What is the definition of the figure parallelism?
 cq6 = """
 SELECT distinct ?Figure ?Definition ?Value
 WHERE {
@@ -81,7 +81,7 @@ WHERE {
     Filter (?FigureName = "Parallelism") }
 """
 
-# Q7: Figures of speech or figures of thought with position beginning
+# Q7: Which figures of speech or figures of thought have their defining element in the beginning?
 cq7 = """
 SELECT distinct ?Figure
 WHERE {
@@ -92,7 +92,7 @@ WHERE {
     Filter ((?GroupName = "FigureOfSpeech" || ?GroupName = "FigureOfThought") && ?PositionLocation = "Beginning") }
 """
 
-# Q8: Figures with a repeated element of the same form.
+# Q8: Which figures have  repeat an element in the same form?
 cq8 = """
 SELECT distinct ?Figure
 WHERE {
@@ -100,7 +100,7 @@ WHERE {
 }
 """
 
-# Q9: Figures where a word in the same form is repeated.
+# Q9: In which figures is a word in the same form repeated?
 cq9 = """
 SELECT distinct ?Figure
 WHERE {
@@ -109,7 +109,7 @@ WHERE {
 }
 """
 
-# Q10: Figures and their opposite figures.
+# Q10: Which figures are the opposite of other figures?
 cq10 = """
 SELECT distinct ?FigureA ?FigureB
 WHERE {
@@ -118,62 +118,62 @@ WHERE {
 """
 
 
-print("Competency Question Q1: Neither a trope nor a figure of construction, but position at the beginning.")
+print("Competency Question Q1: Which rhetorical figures have their defining element in a word?")
 result = g.query(cq1)
 print(f"Number of matching rhetorical figures: {len(result)}")
 for row in result:
     print(row)
 
-print("\nCompetency Question Q2: Rhetorical figures where a letter is omitted.")
+print("\nCompetency Question Q2: In which rhetorical figures is a letter omitted?")
 result = g.query(cq2)
 print(f"Number of matching rhetorical figures: {len(result)}")
 for row in result:
     print(row)
 
-print("\nCompetency Question Q3: Figures that are neither figures of construction, nor figures of speech, "
+print("\nCompetency Question Q3: What are figures that are neither figures of construction, nor figures of speech, nor figures of thought?"
       "nor figures of thought.")
 result = g.query(cq3)
 print(f"Number of matching rhetorical figures: {len(result)}")
 for row in result:
     print(row)
 
-print("\nCompetency Question Q4: All figure that are tropes (should be same output as cq3).")
+print("\nCompetency Question Q4: Which figures belong to the rhetorical group of tropes (should be the same output as Q3)?")
 result = g.query(cq4)
 print(f"Number of matching rhetorical figures: {len(result)}")
 for row in result:
     print(row)
 
-print("\nCompetency Question Q5: Example sentences for the figure Alliteration.")
+print("\nCompetency Question Q5: What is an example sentence for the figure alliteration?")
 result = g.query(cq5)
 print(f"Number of matching rhetorical figures: {len(result)}")
 for row in result:
     print(row)
 
-print("\nCompetency Question Q6: Definition of figure Parallelism.")
+print("\nCompetency Question Q6: What is the definition of the figure parallelism?")
 result = g.query(cq6)
 print(f"Number of matching rhetorical figures: {len(result)}")
 for row in result:
     print(row)
 
-print("\nCompetency Question Q7: Figures of speech and figures of thought with position beginning.")
+print("\nCompetency Question Q7: Which figures of speech or figures of thought have their defining element in the beginning?")
 result = g.query(cq7)
 print(f"Number of matching rhetorical figures: {len(result)}")
 for row in result:
     print(row)
 
-print("\nCompetency Question Q8: Figures with a repeated element of the same form.")
+print("\nCompetency Question Q8: Which figures have  repeat an element in the same form?")
 result = g.query(cq8)
 print(f"Number of matching rhetorical figures: {len(result)}")
 for row in result:
     print(row)
 
-print("\nCompetency Question Q9: Figures where a word in the same form is repeated.")
+print("\nCompetency Question Q9: In which figures is a word in the same form repeated?")
 result = g.query(cq9)
 print(f"Number of matching rhetorical figures: {len(result)}")
 for row in result:
     print(row)
 
-print("\nCompetency Question Q10: All Figures that have opposite figures.")
+print("\nCompetency Question Q10: Which figures are the opposite of other figures?")
 result = g.query(cq10)
 print(f"Number of matching rhetorical figures: {len(result)}")
 for row in result:
